11f8f3907eed75ef9b0e9a70657f55d56014f36e
Cody Beardsley <beardsley.cody@gmail.com>
2018-03-26 16:50:32 -0700

    bugfix for non-displayable character

--- aesthetique.py
+++ aesthetique.py
@@ -8,9 +8,9 @@ class Aesthetique:
 
     def translate(self, input):
         normal = u' 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~'
-        wide = u'　０１２３４５６７８９ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ！゛＃＄％＆（）＊＋、ー。／：；〈＝〉？＠［\\］＾＿‘｛｜｝～'
+        wide = u' ０１２３４５６７８９ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ！゛＃＄％＆（）＊＋、ー。／：；〈＝〉？＠［\\］＾＿‘｛｜｝～'
         widemap = dict((ord(x[0]), x[1]) for x in zip(normal, wide))
         wide_output = input.translate(widemap)
         name_output = Gimei().name.kanji
-        output = u'{0} {1}'.format(wide_output, name_output)
+        output = '{0} {1}'.format(wide_output, name_output)
         return output
